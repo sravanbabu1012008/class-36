435d209ae385e732ed085d9f9b464dae5051cd40
sravanbabu1012008 <81636936+sravanbabu1012008@users.noreply.github.com>
2021-05-24 13:51:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var gameState=0
+var playerCount
+var database
+var form,player,game;
+
+
+function setup(){
+database=firebase.database()
+    createCanvas(400,400);
+  
+    game=new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    
+}
+
